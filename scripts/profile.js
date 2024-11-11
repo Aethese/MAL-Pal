38ce799e9a9673cc7d84220610ba382dfd401389
Aethese <59108437+Aethese@users.noreply.github.com>
2024-11-11 00:57:35 -0600

    use functions more

--- scripts/profile.js
+++ scripts/profile.js
@@ -2,27 +2,36 @@ const daysAnime = document.querySelector("#statistics > div:nth-child(2) > div.s
 const daysManga = document.querySelector("#statistics > div:nth-child(3) > div.stats.manga > div.stat-score.di-t.w100.pt8.mb8 > div.di-tc.al.pl8.fs12.fw-b");
 const totalEntries = document.querySelector("#statistics > div:nth-child(2) > div.stats.anime > div.mt12.ml8.mr8.clearfix > ul.stats-data.fl-r > li:nth-child(1) > span.di-ib.fl-r");
 
+function strToInt(string)
+{
+	// take a number, like 1,042 and return 1042 in int form
+	return parseInt((string).replace(/,/g, ""), 10);
+}
+
+function formatNum(number)
+{
+	return parseFloat(number).toLocaleString(
+		undefined, { minimumFractionDigits: 1, maximumFractionDigits: 1 }
+	);
+}
+
 if (daysAnime)
 {
 	// add hours spent watching next to "Days:" text
 
 	// ANIME:
 	let daysText = daysAnime.childNodes[1].nodeValue;
-	let hoursNum = Number(daysText);
+	let hoursNum = strToInt(daysText);
 	let hours = (hoursNum * 24).toFixed(1);
-	let formattedHours = parseFloat(hours).toLocaleString(
-		undefined, { minimumFractionDigits: 1, maximumFractionDigits: 1 }
-	);
+	let formattedHours = formatNum(hours);
 
 	daysAnime.childNodes[1].nodeValue = daysText + " ("+ formattedHours + " Hours)";
 
 	// MANGA:
 	let mangaDaysText = daysManga.childNodes[1].nodeValue;
-	let mangaHoursNum = Number(mangaDaysText);
+	let mangaHoursNum = strToInt(mangaDaysText);
 	let mangaHours = (mangaHoursNum * 24).toFixed(1);
-	let mangaFormattedHours = parseFloat(mangaHours).toLocaleString(
-		undefined, { minimumFractionDigits: 1, maximumFractionDigits: 1 }
-	);
+	let mangaFormattedHours = formatNum(mangaHours);
 
 	daysManga.childNodes[1].nodeValue = mangaDaysText + " ("+ mangaFormattedHours + " Hours)";
 	console.log("[MAL Pal] Modified days text");
@@ -34,6 +43,7 @@ if (daysAnime)
 if (totalEntries)
 {
 	// add percent a category makes up of the total entries
+
 	const watching = document.querySelector("#statistics > div:nth-child(2) > div.stats.anime > div.mt12.ml8.mr8.clearfix > ul.stats-status.fl-l > li:nth-child(1) > span");
 	const completed = document.querySelector("#statistics > div:nth-child(2) > div.stats.anime > div.mt12.ml8.mr8.clearfix > ul.stats-status.fl-l > li:nth-child(2) > span");
 	const onHold = document.querySelector("#statistics > div:nth-child(2) > div.stats.anime > div.mt12.ml8.mr8.clearfix > ul.stats-status.fl-l > li:nth-child(3) > span");
@@ -41,16 +51,15 @@ if (totalEntries)
 	const ptw = document.querySelector("#statistics > div:nth-child(2) > div.stats.anime > div.mt12.ml8.mr8.clearfix > ul.stats-status.fl-l > li:nth-child(5) > span");
 	const categories = [watching, completed, onHold, dropped, ptw];
 
-	let totalEntriesNum = parseInt((totalEntries.textContent).replace(/,/g, ""), 10);
+	let totalEntriesNum = strToInt(totalEntries.textContent);
 
-	// DISABLED FOR NOW
-	// for (let i = 0; i < categories.length; i++)
-	// {
-	// 	let amount = parseInt((categories[i].textContent).replace(/,/g, ""), 10);
-	// 	let percent = ((amount / totalEntriesNum) * 100).toFixed(1);
-	// 	let previousText = categories[i].textContent;
-	// 	categories[i].textContent = previousText + " (" + percent + "%)";
-	// }
+	for (let i = 0; i < categories.length; i++)
+	{
+		let amount = strToInt(categories[i].textContent);
+		let percent = ((amount / totalEntriesNum) * 100).toFixed(1);
+		let previousText = categories[i].textContent;
+		categories[i].textContent = previousText + " (" + percent + "%)";
+	}
 	console.log("[MAL Pal] Modified total entries text");
 } else
 {
