162f199c676a5d345eea75e4ed5527cb0eefcdc9
Aethese <59108437+Aethese@users.noreply.github.com>
2025-12-23 03:53:07 -0600

    remove junk resize code

--- popup/popup.js
+++ popup/popup.js
@@ -38,16 +38,6 @@ chrome.storage.local.get(['showHoursInHistory'], function(result) {
 
 
 /* Handle dropdown menu animation */
-function forceResize()
-{
-	// bad and not fully working fix to try and help the issue with firefox
-	// not properly resizing the popup window like chromium browsers do
-	setTimeout(() => {
-		document.body.style.height = 'auto';
-		document.documentElement.style.height = 'auto';
-	}, 250);
-}
-
 let dropdowns = document.getElementsByClassName('dropdown');
 for (let i=0; i<dropdowns.length; i++)
 {
@@ -60,7 +50,6 @@ for (let i=0; i<dropdowns.length; i++)
 		if (panel.style.maxHeight)
 		{
 			panel.style.maxHeight = null;
-			forceResize();
 		}
 		else
 		{
